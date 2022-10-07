14c698edbf06abb5e6b4d4601cfdc4a2655a90fe
Syoker <lautarivan@hotmail.com>
2022-10-07 18:57:42 -0300

    deleted alerts

--- js/language.js
+++ js/language.js
@@ -1,9 +1,7 @@
 var userLang = navigator.language || navigator.userLanguage;
 
 if (userLang = "es") {
-    alert ("Página en " + userLang);
     location.href="/es/";
 } else {
-    alert ("Page in " + userLang);
     location.href="/";
 }
