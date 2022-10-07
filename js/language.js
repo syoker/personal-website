28890fcb6e0d60c88cb31e724113802d974e6f84
Syoker <lautarivan@hotmail.com>
2022-10-07 19:24:23 -0300

    fix paths

--- js/language.js
+++ js/language.js
@@ -1,7 +1,7 @@
 var userLang = navigator.language || navigator.userLanguage;
 
 if (userLang = "es") {
-    location.href="/es/";
+    location.href="es/";
 } else {
-    location.href="/";
+    location.href="en/";
 }
