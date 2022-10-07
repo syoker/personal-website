1e7cadf92a95c9ff6200089b9c322f2dd4550249
Syoker <lautarivan@hotmail.com>
2022-10-07 18:47:59 -0300

    added userLang in alerts

--- js/language.js
+++ js/language.js
@@ -1,9 +1,9 @@
 var userLang = navigator.language || navigator.userLanguage;
 
 if (userLang = "es") {
-    alert ("Página en español");
+    alert ("Página en " + userLang);
     location.href="/es/";
 } else {
-    alert ("Page in English");
+    alert ("Page in " + userLang);
     location.href="/";
 }
