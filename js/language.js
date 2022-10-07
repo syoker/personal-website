866a1aadc956ada5c4de8f5caf14df00400b547e
Syoker <lautarivan@hotmail.com>
2022-10-07 18:42:31 -0300

    added alerts

--- js/language.js
+++ js/language.js
@@ -1,7 +1,9 @@
 var userLang = navigator.language || navigator.userLanguage;
 
 if (userLang = "es") {
+    alert ("Página en español");
     location.href="/es/";
 } else {
+    alert ("Page in English");
     location.href="/";
 }
