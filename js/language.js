bb492ccefff1cff7b27ecf625bf6df3b5b813891
Syoker <lautarivan@hotmail.com>
2022-10-08 00:22:38 -0300

    changed method to regexr

--- js/language.js
+++ js/language.js
@@ -1,37 +1,8 @@
-var userLang = navigator.language || navigator.userLanguage;
+// @ts-check
 
-switch (userLang) {
-    case 'es':
-    case 'es-419':
-    case 'es-AR':
-    case 'es-BR':
-    case 'es-BZ':
-    case 'es-CL':
-    case 'es-CO':
-    case 'es-CR':
-    case 'es-CU':
-    case 'es-DO':
-    case 'es-EA':
-    case 'es-EC':
-    case 'es-ES':
-    case 'es-GQ':
-    case 'es-GT':
-    case 'es-HN':
-    case 'es-IC':
-    case 'es-MX':
-    case 'es-NI':
-    case 'es-NI':
-    case 'es-PA':
-    case 'es-PE':
-    case 'es-PH':
-    case 'es-PR':
-    case 'es-PY':
-    case 'es-SV':
-    case 'es-US':
-    case 'es-UY':
-    case 'es-VE':
-        location.href = "./es/";
-        break;
-    default:
-        location.href = "./en/";
-}
+const acceptedLanguages = ["es", "en"];
+
+const matches = navigator.language.match(/^([a-z]{2})(?:-[a-zA-Z]{2})?(?:-[0-9]{3})?$/);
+const language = matches ? matches[1] : "en";
+
+location.href = acceptedLanguages.includes(language) ? `./${language}/` : "./en/";
