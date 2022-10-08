5e5850a1eb44add21715df417a575bf60054e140
Syoker <lautarivan@hotmail.com>
2022-10-08 00:40:44 -0300

    added logs

--- js/language.js
+++ js/language.js
@@ -5,4 +5,8 @@ const acceptedLanguages = ["es", "en"];
 const matches = navigator.language.match(/^([a-z]{2})(?:-[a-zA-Z]{2})?(?:-[0-9]{3})?$/);
 const language = matches ? matches[1] : "en";
 
+console.log(matches);
+console.log(language);
+console.log(acceptedLanguages.includes(language) ? "true" : "false");
+
 location.href = acceptedLanguages.includes(language) ? `./${language}/` : "./en/";
