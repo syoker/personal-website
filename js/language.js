620f206a7602e08b80fcdfea0edb646d1e7a6327
Syoker <lautarivan@hotmail.com>
2022-10-16 23:02:28 -0300

    added new condition

--- js/language.js
+++ js/language.js
@@ -1,8 +1,7 @@
-// @ts-check
-
-const acceptedLanguages = ["es", "en"];
+const acceptedLanguages = ["es"];
 
 const matches = navigator.language.match(/^([a-z]{2})(?:-[a-zA-Z]{2})?(?:-[0-9]{3})?$/);
 const language = matches ? matches[1] : "en";
 
-location.href = acceptedLanguages.includes(language) ? `./${language}/` : "./en/";
+if (currentLanguage != language)
+    location.href = acceptedLanguages.includes(language) ? `./${language}/` : `${currentPage}/`;
