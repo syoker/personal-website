160521ed519338952304500a6871672c48166c47
Syoker <lautarivan@hotmail.com>
2022-12-20 20:27:28 -0300

    added verify condition

--- js/language.js
+++ js/language.js
@@ -3,5 +3,7 @@ const acceptedLanguages = ["es"];
 const matches = navigator.language.match(/^([a-z]{2})(?:-[a-zA-Z]{2})?(?:-[0-9]{3})?$/);
 const language = matches ? matches[1] : "en";
 
-if (currentLanguage != language)
-    location.href = acceptedLanguages.includes(language) ? `/${language}${currentPage}/` : `${currentPage}/`;
+const verify = acceptedLanguages.includes(language) ? language : "en";
+
+if (currentLanguage != verify)
+    location.href = acceptedLanguages.includes(verify) ? `/${verify}${currentPage}/` : `${currentPage}/`;
