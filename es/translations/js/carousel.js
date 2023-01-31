1ab52537e7a7a2279af484eaf41a436506734539
Syoker <lautarivan@hotmail.com>
2023-01-31 18:06:27 -0300

    added fansub page

---
+++ es/translations/js/carousel.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const row = document.querySelectorAll('.row');
+const pointer = document.querySelectorAll('.pointer');
+const containerCards = document.querySelector('.containerCards');
+
+row.forEach(( perRow , i )=> {
+    row[i].addEventListener('click',()=> {
+        let position = i
+        let newPosition = position * -50
+
+        containerCards.style.transform = `translateX(${ newPosition }%)`
+
+        pointer.forEach(( perPointer , i )=>{
+            pointer[i].classList.remove('active')
+        })
+        pointer[i].classList.add('active')
+    })
+})
