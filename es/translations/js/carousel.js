82984cb9b93dbc5b6d05d8900d62065d7be28e9f
Syoker <lautarivan@hotmail.com>
2023-02-01 00:25:02 -0300

    changed things

--- es/translations/js/carousel.js
+++ es/translations/js/carousel.js
@@ -1,19 +1,17 @@
-'use strict'
+const row = document.querySelectorAll(".row");
+const pointer = document.querySelectorAll(".pointer");
+const containerCards = document.querySelector(".containerCards");
 
-const row = document.querySelectorAll('.row');
-const pointer = document.querySelectorAll('.pointer');
-const containerCards = document.querySelector('.containerCards');
+pointer.forEach((perPointer, i) => {
+  row[i].addEventListener("click", () => {
+    let position = i;
+    let newPosition = position * -50;
 
-row.forEach(( perRow , i )=> {
-    row[i].addEventListener('click',()=> {
-        let position = i
-        let newPosition = position * -50
+    containerCards.style.transform = `translateX(${newPosition}%)`;
 
-        containerCards.style.transform = `translateX(${ newPosition }%)`
-
-        pointer.forEach(( perPointer , i )=>{
-            pointer[i].classList.remove('active')
-        })
-        pointer[i].classList.add('active')
-    })
-})
+    pointer.forEach((perPointer, i) => {
+      pointer[i].classList.remove("active");
+    });
+    pointer[i].classList.add("active");
+  });
+});
