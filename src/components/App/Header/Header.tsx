6a8db1f4c03bfda2f9e29712888dc29ae71128e5
Syoker <lautarivan@hotmail.com>
2023-06-23 15:46:28 -0300

    fixed responsive bugs

--- src/components/App/Header/Header.tsx
+++ src/components/App/Header/Header.tsx
@@ -1,83 +1,15 @@
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import { MdKeyboardArrowLeft } from 'react-icons/md';
-
-import { ValidPages } from 'src/types';
-import { useWindowDimensions } from 'src/hooks';
-
-import Link from 'next/link';
-import Image from 'next/image';
-import classNames from 'classnames';
-import useTranslation from 'next-translate/useTranslation';
-
-import styles from './styles/Header.module.css';
+import HeaderMobile from './HeaderMobile';
+import HeaderDesktop from './HeaderDesktop';
 
 export default function Header({ pathname }: { pathname: string }) {
-	const { t } = useTranslation('header');
-	const { width } = useWindowDimensions();
-
-	const [windowWidth, setWindowWidth] = useState<number>(0);
-
-	useEffect(() => {
-		setWindowWidth(width);
-	}, [width]);
-
-	const validPages: ValidPages = {
-		'/blog/motorolaonefusion': <h2>{'Motorola One Fusion'}</h2>,
-		'/blog/motorolaonefusion/guide-flash-official-firmware': <h2>{`[${t('guide')}] Flash Official Firmware`}</h2>,
-		'/blog/motorolaonefusion/guide-unlock-bootloader': <h2>{`[${t('guide')}] Unlock Bootloader`}</h2>,
-		'/blog/motorolaonefusion/guide-flash-custom-recovery': <h2>{`[${t('guide')}] Flash Custom Recovery`}</h2>,
-		'/blog/motorolaonefusion/guide-flash-rom-gsi': <h2>{`[${t('guide')}] Flash ROM GSI`}</h2>,
-	};
-
-	const errorPage = <h2>{'Error 404'}</h2>;
-
-	const router = useRouter();
-
-	const home = () => router.push('../../../');
-
-	const back = () => {
-		if (pathname === '/blog/motorolaonefusion') {
-			router.push('/blog');
-			return;
-		}
-
-		if (pathname.includes('/blog/motorolaonefusion/guide')) {
-			router.push('/blog/motorolaonefusion');
-			return;
-		}
-
-		home();
-	};
-
-	if (windowWidth < 1280 && (pathname === '/' || pathname === '/blog' || pathname === '/projects')) {
-		return null;
+	if (pathname === '/' || pathname === '/blog' || pathname === '/projects') {
+		return <HeaderDesktop pathname={pathname} />;
 	}
 
 	return (
-		<header className={styles.header}>
-			<MdKeyboardArrowLeft onClick={back} className={styles.icon} />
-			{validPages[pathname] || errorPage}
-			<Image
-				className={styles.avatar}
-				src="/icon/avatar.svg"
-				alt="Syoker"
-				width={200}
-				height={200}
-				onClick={home}
-				priority
-			/>
-			<nav className={styles.navigation}>
-				<Link className={classNames(styles.link, pathname.includes('blog') && styles.linkSelected)} href="../../blog">
-					{t('blog-title')}
-				</Link>
-				<Link
-					className={classNames(styles.link, pathname.includes('projects') && styles.linkSelected)}
-					href="../../projects"
-				>
-					{t('projects-title')}
-				</Link>
-			</nav>
-		</header>
+		<>
+			<HeaderMobile pathname={pathname} />
+			<HeaderDesktop pathname={pathname} />
+		</>
 	);
 }
--- src/components/shared/CustomCard/CustomCard.tsx
+++ src/components/shared/CustomCard/CustomCard.tsx
@@ -1,8 +1,6 @@
 import { useRouter } from 'next/router';
 import { Card, CardActionArea } from '@mui/material';
 
-import { useWindowDimensions } from 'src/hooks';
-
 import Image from 'next/image';
 
 import styles from './styles/CustomCard.module.css';
@@ -13,21 +11,18 @@ interface CustomCardProps {
 	description: string;
 	link: string;
 	titleSide?: string;
-	sx?: { width: string };
+	sx?: { width?: string };
 }
 
 export default function CustomCard(props: CustomCardProps) {
 	const { banner, title, description, link, titleSide, sx } = props;
-	const { width } = useWindowDimensions();
 
 	const router = useRouter();
 
-	const url = () => router.push(link);
-
-	const cardStyle = width < 1280 ? { width: '100%' } : sx ? sx : { width: '100%' };
+	const cardStyle = sx ? sx : { width: '100%' };
 
 	return (
-		<Card variant="outlined" sx={cardStyle} onClick={url} className={styles.card}>
+		<Card variant="outlined" sx={cardStyle} onClick={() => router.push(link)} className={styles.card}>
 			<CardActionArea className={styles.container}>
 				<Image className={styles.banner} src={banner} alt={`${title} banner`} width={600} height={300} priority />
 				<div className={styles.data}>
